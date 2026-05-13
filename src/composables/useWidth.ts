946a5148552feb97d959ba6f4ddd2d1fc88d111e
Артур Аюпов <m1rariel@MacBook-Air-Artur.local>
2026-05-13 20:58:23 +0500

    fix: fix alert width

--- src/composables/useWidth.ts
+++ src/composables/useWidth.ts
@@ -1,7 +1,7 @@
 import { useWindowSize } from "@vueuse/core";
 const { width } = useWindowSize();
 
-export const useSmallWidth = (_breakpoint = 1024) => {
+export const useSmallWidth = (value = 1024) => {
   const isSmallWindow = width.value < 1024;
 
   return isSmallWindow;
