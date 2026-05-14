cfa71b498992c4c74e739882ee3cf832c23b4d45
Артур Аюпов <m1rariel@MacBook-Air-Artur.local>
2026-05-14 20:12:57 +0500

    fix: fix return types

--- src/api/todo/getTodos.ts
+++ src/api/todo/getTodos.ts
@@ -1,7 +1,7 @@
 import type { Todo } from "@/types/todo";
 import { useAxios } from "../useAxios";
 
-export const getTodos = async (): Promise<Todo[]> => {
+export const getTodos = async () => {
   const axiosInstance = useAxios();
   const response = await axiosInstance.get<Todo[]>("/todos", {
     params: { _limit: 10 },
